4a165179613857e9ee174bb81b6e9fed494a9a59
shahulyn <103170766+shahulyn@users.noreply.github.com>
2024-06-02 13:27:04 +0500

    Update conf.docker.sh

--- conf.docker.sh
+++ conf.docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-json=$(curl -s https://raw.githubusercontent.com/sajjaddg/xray-reality/master/config.json)
+json=$(curl -s https://raw.githubusercontent.com/shahulyn/xray-reality-mv/master/config.json)
 
 keys=$(xray x25519)
 pk=$(echo "$keys" | awk '/Private key:/ {print $3}')
@@ -7,7 +7,7 @@ pub=$(echo "$keys" | awk '/Public key:/ {print $3}')
 serverIp=$(curl -s ifconfig.me)
 uuid=$(xray uuid)
 shortId=$(openssl rand -hex 8)
-url="vless://$uuid@$serverIp:443?path=%2F&security=reality&encryption=none&pbk=$pub&fp=chrome&type=http&sni=yahoo.com&sid=$shortId#IRVLESS-REALITY-04"
+url="vless://$uuid@$serverIp:443?path=%2F&security=reality&encryption=none&pbk=$pub&fp=chrome&type=http&sni=&sid=$shortId#IRVLESS-REALITY-04"
 
 newJson=$(echo "$json" | jq \
     --arg pk "$pk" \
